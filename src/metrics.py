9ff0ff847847e32f6a0db6c02c7f9e5d6a393f84
Abhishek Rai <rai.abhishek90@gmail.com>
2023-04-08 11:09:03 -0700

    chore: fix package version
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,4 @@
-from prometheus_client import generate_latest, Summary
+from prometheus_client import Summary
 
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 METRICS_MIME_TYPE = str('text/plain; version=0.0.4; charset=utf-8')
