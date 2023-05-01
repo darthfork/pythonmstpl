a5dad9de1d45e285dd08f0b4a324b6a7ace0872d
Abhishek Rai <rai.abhishek90@gmail.com>
2023-05-01 09:42:36 -0700

    chore: mirror requirements.txt packages under setup.py as install_requires
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 from pythonmstpl.version import VERSION
 
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='pythonmstpl',
     version=VERSION,
     description='Python micro-service template app',
     packages=find_packages(exclude=["test"]),
+    install_requires=install_requires,
 )
